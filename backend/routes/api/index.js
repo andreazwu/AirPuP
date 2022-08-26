d0326daa22b368ec984996c95c36709610c03eb3
Andrea Wu <angeeya.wu@gmail.com>
2022-08-26 17:47:01 -0500

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,18 +1,28 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
-// const { setTokenCookie } = require('../../utils/auth.js');
-// const { User } = require('../../db/models');
+
 
 // Connect restoreUser middleware to the API router
 // If current user session is valid, set req.user to the user in the database
 // If current user session is not valid, set req.user to null
 router.use(restoreUser);
 
+router.use('/session', sessionRouter);
 
-// router.post('/test', function (req, res) {
-//   res.json({ requestBody: req.body });
-// });
+router.use('/users', usersRouter);
 
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
+
+
+
+
+// // Testing Auth:
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
 
 // // GET /api/set-token-cookie
 // router.get('/set-token-cookie', async (_req, res) => {
@@ -25,7 +35,6 @@ router.use(restoreUser);
 //   return res.json({ user });
 // });
 
-
 // // GET /api/restore-user
 // router.get(
 //   '/restore-user',
@@ -33,7 +42,6 @@ router.use(restoreUser);
 //     return res.json(req.user);
 //   }
 // );
-
 
 // // GET /api/require-auth
 // const { requireAuth } = require('../../utils/auth.js');
@@ -44,8 +52,6 @@ router.use(restoreUser);
 //     return res.json(req.user);
 //   }
 // );
-
-
 
 
 module.exports = router;
