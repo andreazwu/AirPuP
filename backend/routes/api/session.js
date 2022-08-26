0dcf6c6d96a52d9857b8e69a686caf6520ea4206
Andrea Wu <angeeya.wu@gmail.com>
2022-08-26 17:51:01 -0500

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -28,6 +28,14 @@ router.post(
   }
 );
 
+// Log out
+router.delete(
+  '/',
+  (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
 
 
 module.exports = router;
