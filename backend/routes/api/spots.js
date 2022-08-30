796275a6ba4937f2bb3eeae4e760d582debc389d
Andrea Wu <angeeya.wu@gmail.com>
2022-08-29 21:33:03 -0500

    get all spots owned by the current user

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -12,11 +12,11 @@ const { Op } = require('sequelize');
 
 // Get all spots
 // id, ownerId, address, city, state, country, lat, lng, name, description, price, createdAt, updatedAt, >>>previewImage, and >>>avgRating
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   const spots = await Spot.findAll({
     // include: [
     //   { model: Review, attributes: [] },
-    //   { model: SpotImage, attributes: ["url"], where: { preview: true } }
+    //   { model: SpotImage, attributes: [["url", "previewImage"], where: { preview: true } }
     // ],
     // attributes: {
     //   include: [
@@ -25,7 +25,34 @@ router.get('/', async (req, res) => {
     // }
   })
   return res.json(spots)
-});
+})
+
+
+// Get all Spots owned by the Current User
+router.get('/current', requireAuth, async (req, res) => {
+  //requireAuth ensures (req.user) -- if no current logged in user, err-->err handling
+  const currentUser = req.user
+
+  const currentSpots = await Spot.findAll({
+    where: {
+      ownerId: currentUser.id
+    }
+  })
+
+  return res.json(currentSpots)
+})
+
+
+
+// Create a Spot
+// Creates and returns a new spot.
+
+//  An authenticated user is required for a successful response
+//  New spot exists in the database after request
+//  Spot data returned includes the id, ownerId, address, city, state, country, lat, lng, name, description, price, createdAt, and updatedAt
+//  Error response with status 400 is given when body validations for the address, city, state, country, lat, lng, name, description, or price are violated
+router.post("/")
+
 
 
 module.exports = router;
