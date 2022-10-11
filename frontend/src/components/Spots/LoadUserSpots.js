49f0575d34ca4918cb2d565f2a5b5bb8d045b82f
Andrea Wu <angeeya.wu@gmail.com>
2022-10-11 12:39:11 -0500

    load user spots component; need to implement myspot

--- frontend/src/components/Spots/LoadUserSpots.js
+++ frontend/src/components/Spots/LoadUserSpots.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { getUserSpots } from "../../store/spots";
+//import MySpot
 import "./LoadUserSpots.css"
 
 
@@ -25,23 +26,22 @@ const LoadUserSpots = () => {
   return (
     <>
       <div className="whole-container">
-        {/* {
-          spotsArr.map((spot) => {
-            <div key={spot.id} className="LoadUserSpots-wrapper">
-              {console.log("4 (2.5) RETURN:", spot)}
-              {spot}
-            </div>
-          })
-        } */}
-        <div className="spot-container">
+        <h1>My Spots</h1>
+      </div>
+
+      <div className="spot-container">
           {
+            spotsArr.length === 0 ?
+
+            <div>You're not currently hosting any spots.</div> :
+
             spotsArr.map((spot) => (
               //implement spot in separate component; add prop
-              <Spot key={spot.id} spot={spot}></Spot>
+              <MySpot key={spot.id} spot={spot}></MySpot>
             ))
           }
-        </div>
       </div>
+
     </>
   )
 }
