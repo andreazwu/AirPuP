25fb2617a3bda524d0ca097005808b364d86e003
Andrea Wu <angeeya.wu@gmail.com>
2022-10-14 12:51:49 -0500

    css

--- frontend/src/components/Spots/MySpot.js
+++ frontend/src/components/Spots/MySpot.js
@@ -24,7 +24,7 @@ const MySpot = ({spot}) => {
         <div className="allspots-spot-image-container">
           {spot.previewImage ?
             (<div><img src={spot.previewImage} /></div>) :
-            (<div><img src="https://bitsofco.de/content/images/2018/12/broken-1.png" alt="spot has no preview image" /></div>)
+            (<div><img src="../images/no-image.png" alt="spot has no preview image" /></div>)
           }
         </div>
 
